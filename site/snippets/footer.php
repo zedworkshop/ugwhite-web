9fa1611f59c04775bcec581c36768e9267c84162
Adam Campbell <adam@hotmeteor.com>
2015-12-09 10:46:01 -0600

    Footer updates

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -5,7 +5,8 @@
 		<div class="container">
 			<div class="clearfix center">
 				<div class="md-col md-col-5 md-left-align px2 py2">
-					<p class="small white normal futura">Copyright &copy; <?php echo date('Y') ?> U.G. White Mercantile</p>
+					<p class="small white normal futura mb0 mt1">Copyright &copy; <?php echo date('Y') ?> U.G. White Mercantile</p>
+					<p class="small white futura mb0">Athens - (256) 232-4540 &middot; Huntsville - (256) 907-1917</p>
 				</div>
 
 				<div class="md-show md-col md-col-2 mtn2 footer-logo">
@@ -13,7 +14,7 @@
 				</div>
 
 				<div class="md-col md-col-5 md-right-align px2 py2">
-					<p class="small white normal futura">
+					<p class="small white normal futura mb0">
 						<a class="btn small" href="<?php echo url() ?>">Home</a>
 						<?php foreach($pages->visible() as $p): ?>
 						<a class="btn small" href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a>
@@ -21,9 +22,9 @@
 					</p>
 				</div>
 			</div>
-			<div class="clearfix center md-right-align">
-				<div class="sm-col sm-col-12 px2 mtn1">
-					<ul class="list-reset">
+			<div class="clearfix">
+				<div class="sm-col sm-col-12 center md-right-align px2">
+					<ul class="list-reset mb0">
 					  	<li class="inline-block px1">
 					    	<a href="<?php echo $site->facebook() ?>" target="_blank" class="white">
 					    		<svg class="icon-facebook icon-2x"><use xlink:href="#icon-facebook"></use></svg>
