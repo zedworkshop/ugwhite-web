85849d7868214b542e24acfc72d5ccbbe506ef93
Adam Campbell <adam@hotmeteor.com>
2015-12-11 07:15:41 -0600

    Home page updates, added links to locations

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -9,13 +9,13 @@
 
 	<div class="inline-block middle bold px2 sunbeam">
 		<span class="inline-block">
-			<span class="sm-show">Athens</span> (256) 232-4540
+			<span class="sm-show"><a class="sunbeam" href="https://www.google.com/maps/place/101+N+Jefferson+St,+Athens,+AL+35611/@34.8035306,-86.974716,17z/data=!3m1!4b1!4m2!3m1!1s0x88628b5d4d630759:0x4b1bc01ca5938d10?hl=en" target="_blank">Athens</a></span> (256) 232-4540
 		</span>
 	</div>
 
 	<div class="inline-block middle bold px2 sunbeam">
 		<span class="inline-block">
-			<span class="sm-show">Huntsville</span> (256) 907-1917
+			<span class="sm-show"><a class="sunbeam" href="https://www.google.com/maps/place/U+G+White+Mercantile/@34.731903,-86.5884407,17z/data=!3m1!4b1!4m7!1m4!3m3!1s0x88626b56b2d0891b:0x1459ac70b89e9191!2s115+Clinton+Ave+E,+Huntsville,+AL+35801!3b1!3m1!1s0x88626b56b2d0891b:0x8699ecc4c9a5a8ee?hl=en" target="_blank">Huntsville</a></span> (256) 907-1917
 		</span>
 	</div>
 
--- site/templates/home.php
+++ site/templates/home.php
@@ -1,160 +1,136 @@
 <?php snippet('header') ?>
-
-  	<main class="main" role="main">
-
-	  	<!-- Hero -->
-		<section class="[ border-box center bg-cover cushion-top-lg cushion-bottom-lg ] [ flex flex-center ] home-hero">
-
-			<div class="col-12 md-col-6 px2 mx-auto">
-				<div class="sm-show sm-col-4 xl-col-4 mx-auto mb2">
-					<img alt="<?php echo $site->title()->html() ?>" src="<?php echo url('assets/images/UGW-logo-tri-rust.png') ?>" />
-				</div>
-
-				<div class="px2 home-tagline">
-					<img alt="We've got the goods to get you there!" src="<?php echo url('assets/images/home-hero-tagline.png') ?>" />
-				</div>
-			</div>
-
-		</section>
-		<!--// Hero -->
-
-		<!-- About -->
-		<section class="[ bg-rust py2 ] home-intro">
-
-			<div class="[ container px2 ] [ center white ]">
-
-				<header class="py1 home-since">
-					<img alt="Since 1917" src="<?php echo url('assets/images/home-since.png') ?>" />
-					<h2 class="mt3">At U.G. White, we pride ourselves <br />in offering quality goods <br />with the best possible customer service.</h2>
-					<img alt="" src="<?php echo url('assets/images/home-bird.png') ?>" />
-				</header>
-
-				<div class="clearfix py3">
-
-					<header>
-						<h3 class="banner banner-md banner-sand mt0 mb3"><span class="text wisdom">Our Provisions</span></h3>
-					</header>
-
-					<div class="col col-6 lg-col-3 px2">
-						<ul class="[ fairview h3 uppercase ] list-reset">
-							<li>Aladdin Oil Lamps</li>
-							<li>Badger Balms &amp; Oinments</li>
-							<li>Colonel Littleton Products</li>
-							<li>Droll Yankee Bird Feeders</li>
-							<li>Fits Socks</li>
-							<li>Mojo</li>
-							<li>Swiss Army Knives</li>
-							<li>Troutman Rockers</li>
-							<li>Camping &amp; Trail Supplies</li>
-							<li>Canning &amp; Preserving</li>
-							<li>Colonial Candles</li>
-						</ul>
-					</div>
-					<div class="col col-6 lg-col-3 px2">
-						<ul class="[ fairview h3 uppercase ] list-reset">
-							<li>Galvanized Wares</li>
-							<li>Kitchen Cutlery</li>
-							<li>Lanterns</li>
-							<li>Local Honey</li>
-							<li>Jams &amp; Jellies</li>
-							<li>Old-fashioned Candy</li>
-							<li>Pottery Churns</li>
-							<li>Tin &amp; Wood Signs</li>
-							<li>Traditional Housewares</li>
-							<li>Walking Sticks</li>
-							<li>Wood &amp; Tin Toys</li>
-						</ul>
-					</div>
-
-					<div class="col col-12 lg-col-6">
-
-						<figure class="px2 py1 m0 inline-block middle">
-							<img alt="Case logo" src="<?php echo url('assets/images/logo-case.png') ?>" />
-						</figure>
-						<figure class="px2 py1 m0 inline-block middle">
-							<img alt="Filson logo" src="<?php echo url('assets/images/logo-filson.png') ?>" />
-						</figure>
-						<figure class="px2 py1 m0 inline-block middle">
-							<img alt="Patagonia logo" src="<?php echo url('assets/images/logo-patagonia.png') ?>" />
-						</figure>
-						<figure class="px2 py1 m0 inline-block middle">
-							<img alt="Tilley logo" src="<?php echo url('assets/images/logo-tilley.png') ?>" />
-						</figure>
-						<figure class="px2 py1 m0 inline-block middle">
-							<img alt="Woolrich logo" src="<?php echo url('assets/images/logo-woolrich.png') ?>" />
-						</figure>
-						<figure class="px2 py1 m0 inline-block middle">
-							<img alt="Grand Trunk logo" src="<?php echo url('assets/images/logo-grandtrunk.png') ?>" />
-						</figure>
-						<figure class="px2 py1 m0 inline-block middle">
-							<img alt="Radio Flyer logo" src="<?php echo url('assets/images/logo-radioflyer.png') ?>" />
-						</figure>
-						<figure class="px2 py1 m0 inline-block middle">
-							<img alt="Lodge logo" src="<?php echo url('assets/images/logo-lodge.png') ?>" />
-						</figure>
-
-						<p class="futura mt2 h6">and many more fine products...</p>
-
-					</div>
-				</div>
-			</div>
-
-		</section>
-		<!-- About -->
-
-		<!-- Locations -->
-		<section class="[ bg-alt-3 ] home-locations">
-
-			<div class="[ container-wide ] [ center white ]">
-
-				<div class="clearfix">
-
-					<div class="col col-12 xl-col-right xl-col-5 px2 py3">
-
-						<header class="py1 center">
-							<h5 class="banner banner-forest mb0 mt0"><span class="text wisdom">Visit us in store</span></h5>
-						</header>
-
-						<div class="center clearfix mb2">
-
-							<div class="sm-col sm-col-6 xl-col-6 px2 py1">
-								<h4 class="sunbeam uppercase futura h3 bold mb0 mt0">Athens</h4>
-								<p class="mb0 futura h6">101 North Jefferson Street</p>
-								<p class="xsmall">
-									Monday - Wednesday, 9am - 6pm<br />
-									Thursday - Saturday, 9am - 8pm
-								</p>
-							</div>
-
-							<div class="sm-col sm-col-6 xl-col-6 px2 py1">
-								<h4 class="sunbeam uppercase futura h3 bold mb0 mt0">Huntsville</h4>
-								<p class="mb0 futura h6">115 Clinton Ave E</p>
-								<p class="xsmall">
-									Monday - Wednesday, 10am - 6pm<br />
-									Thursday - Saturday, 10am - 8pm<br />
-									Sunday, 1pm - 6pm
-								</p>
-							</div>
-
-						</div>
-
-						<a href="<?php echo url('our-history') ?>" class="btn btn-fancy btn-rust btn-lg">Learn about the history of our stores</a>
-
-					</div>
-
-					<div class="[ mx-auto md-col-8 xl-col xl-col-7 ] home-photo">
-
-						<figure class="photo curled tilted">
-							<img alt="UG White Athens storefront" src="<?php echo url('assets/images/img-exterior.jpg') ?>" />
-						</figure>
-
-					</div>
-
-				</div>
-			</div>
-
-		</section>
-
-  	</main>
-
-<?php snippet('footer') ?>
+    <main class="main" role="main">
+        <!-- Hero -->
+        <section class="[ border-box center bg-cover cushion-top-lg cushion-bottom-lg ] [ flex flex-center ] home-hero">
+            <div class="col-12 md-col-6 px2 mx-auto">
+                <div class="sm-show sm-col-4 xl-col-4 mx-auto mb2">
+                    <img alt="<?php echo $site->title()->html() ?>" src="<?php echo url('assets/images/UGW-logo-tri-rust.png') ?>" />
+                </div>
+                <div class="px2 home-tagline">
+                    <img alt="We've got the goods to get you there!" src="<?php echo url('assets/images/home-hero-tagline.png') ?>" />
+                </div>
+            </div>
+        </section>
+        <!--// Hero -->
+        <!-- About -->
+        <section class="[ bg-rust py2 ] home-intro">
+            <div class="[ container px2 ] [ center white ]">
+                <header class="py1 home-since">
+                    <img alt="Since 1917" src="<?php echo url('assets/images/home-since.png') ?>" />
+                    <h2 class="mt3">At U.G. White, we pride ourselves <br />in offering quality goods <br />with the best possible customer service.</h2>
+                    <img alt="" src="<?php echo url('assets/images/home-bird.png') ?>" />
+                </header>
+                <div class="clearfix py3">
+                    <header>
+                        <h3 class="banner banner-md banner-sand mt0 mb3"><span class="text wisdom">Our Provisions</span></h3>
+                    </header>
+                    <div class="col col-6 lg-col-3 px2">
+                        <ul class="[ fairview h3 uppercase ] list-reset">
+                            <li>Aladdin Oil Lamps</li>
+                            <li>Badger Balms &amp; Oinments</li>
+                            <li>Colonel Littleton Products</li>
+                            <li>Droll Yankee Bird Feeders</li>
+                            <li>Fits Socks</li>
+                            <li>Mojo</li>
+                            <li>Swiss Army Knives</li>
+                            <li>Troutman Rockers</li>
+                            <li>Camping &amp; Trail Supplies</li>
+                            <li>Canning &amp; Preserving</li>
+                            <li>Colonial Candles</li>
+                        </ul>
+                    </div>
+                    <div class="col col-6 lg-col-3 px2">
+                        <ul class="[ fairview h3 uppercase ] list-reset">
+                            <li>Galvanized Wares</li>
+                            <li>Kitchen Cutlery</li>
+                            <li>Lanterns</li>
+                            <li>Local Honey</li>
+                            <li>Jams &amp; Jellies</li>
+                            <li>Old-fashioned Candy</li>
+                            <li>Pottery Churns</li>
+                            <li>Tin &amp; Wood Signs</li>
+                            <li>Traditional Housewares</li>
+                            <li>Walking Sticks</li>
+                            <li>Wood &amp; Tin Toys</li>
+                        </ul>
+                    </div>
+                    <div class="col col-12 lg-col-6">
+                        <figure class="px2 py1 m0 inline-block middle">
+                            <img alt="logo-case.png" src="<?php echo url('assets/images/logo-case.png') ?>" />
+                        </figure>
+                        <figure class="px2 py1 m0 inline-block middle">
+                            <img alt="logo-filson.png" src="<?php echo url('assets/images/logo-filson.png') ?>" />
+                        </figure>
+                        <figure class="px2 py1 m0 inline-block middle">
+                            <img alt="logo-patagonia.png" src="<?php echo url('assets/images/logo-patagonia.png') ?>" />
+                        </figure>
+                        <figure class="px2 py1 m0 inline-block middle">
+                            <img alt="logo-tilley.png" src="<?php echo url('assets/images/logo-tilley.png') ?>" />
+                        </figure>
+                        <figure class="px2 py1 m0 inline-block middle">
+                            <img alt="logo-woolrich.png" src="<?php echo url('assets/images/logo-woolrich.png') ?>" />
+                        </figure>
+                        <figure class="px2 py1 m0 inline-block middle">
+                            <img alt="logo-grandtrunk.png" src="<?php echo url('assets/images/logo-grandtrunk.png') ?>" />
+                        </figure>
+                        <figure class="px2 py1 m0 inline-block middle">
+                            <img alt="logo-radioflyer.png" src="<?php echo url('assets/images/logo-radioflyer.png') ?>" />
+                        </figure>
+                        <figure class="px2 py1 m0 inline-block middle">
+                            <img alt="logo-lodge.png" src="<?php echo url('assets/images/logo-lodge.png') ?>" />
+                        </figure>
+                        <p class="futura mt2 h6">and many more fine products...</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- About -->
+        <!-- Locations -->
+        <section class="[ bg-alt-3 ] home-locations">
+            <div class="[ container-wide ] [ center white ]">
+                <div class="clearfix">
+                    <div class="col col-12 xl-col-right xl-col-5 px2 py3">
+                        <header class="py1 center">
+                            <h5 class="banner banner-forest mb0 mt0"><span class="text wisdom">Visit us in store</span></h5>
+                        </header>
+                        <div class="center clearfix mb2">
+                            <div class="sm-col sm-col-6 xl-col-6 px2 py1">
+                                <h4 class="sunbeam uppercase futura h3 bold mb0 mt0">Athens</h4>
+                                <p class="mb1 futura h6">
+                                    <a class="white" href="https://www.google.com/maps/place/101+N+Jefferson+St,+Athens,+AL+35611/@34.8035306,-86.974716,17z/data=!3m1!4b1!4m2!3m1!1s0x88628b5d4d630759:0x4b1bc01ca5938d10?hl=en" target="_blank">
+                                    101 North Jefferson Street
+                                    <br />Athens, AL 35611
+                                    </a>
+                                </p>
+                                <p class="xsmall">
+                                    Monday - Wednesday, 9am - 6pm
+                                    <br /> Thursday - Saturday, 9am - 8pm
+                                </p>
+                            </div>
+                            <div class="sm-col sm-col-6 xl-col-6 px2 py1">
+                                <h4 class="sunbeam uppercase futura h3 bold mb0 mt0">Huntsville</h4>
+                                <p class="mb1 futura h6">
+                                    <a class="white" href="https://www.google.com/maps/place/U+G+White+Mercantile/@34.731903,-86.5884407,17z/data=!3m1!4b1!4m7!1m4!3m3!1s0x88626b56b2d0891b:0x1459ac70b89e9191!2s115+Clinton+Ave+E,+Huntsville,+AL+35801!3b1!3m1!1s0x88626b56b2d0891b:0x8699ecc4c9a5a8ee?hl=en" target="_blank">
+                                    115 Clinton Ave E
+                                    <br />Huntsville, AL 35801
+                                    </a>
+                                </p>
+                                <p class="xsmall">
+                                    Monday - Wednesday, 10am - 6pm
+                                    <br /> Thursday - Saturday, 10am - 8pm
+                                    <br /> Sunday, 1pm - 6pm
+                                </p>
+                            </div>
+                        </div>
+                        <a href="<?php echo url('our-history') ?>" class="btn btn-fancy btn-rust btn-lg">Learn about the history of our stores</a>
+                    </div>
+                    <div class="[ mx-auto md-col-8 xl-col xl-col-7 ] home-photo">
+                        <figure class="photo curled tilted">
+                            <img alt="UG White Athens storefront" src="<?php echo url('assets/images/img-exterior.jpg') ?>" />
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <?php snippet('footer') ?>
